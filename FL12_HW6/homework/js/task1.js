e839666632139a984cd7e2a90c02ab8f972a2715
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-12-24 00:11:40 +0200

    task1 done

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,37 +1,19 @@
-// Your code goes here
-// Implement app for solve Quadratic equation.
-// 	App workflow:
-// 1.	User input 3 values (a, b, c) for quadratic equation (ax2 + bx + c = 0).
-// 2.	If input data not valid show message in console (‘Invalid input data’).
-// 3.	If possible, find Discriminant.
-// 4.	Use console.log to show result, variants:
-// -	x = 0;
-// -	x1 = ‘value1’ and x2 = ‘value2’;
-// -	no solution (when Discriminant < 0);
-
-let a = parseFloat(
-    prompt('please enter "a" to solve quadratic equation (ax2 + bx + c = 0)')
-);
-if (a === "" || isNaN(a)) {
-    alert("‘Invalid input data’");
+const a = +prompt('Please enter value for "a" to solve Quadratic Equation (ax2 + bx + c = 0)');
+const b = +prompt('Please enter value for "b" to solve Quadratic Equation (ax2 + bx + c = 0)');
+const c = +prompt('Please enter value for "c" to solve Quadratic Equation (ax2 + bx + c = 0)');
+const four = 4;
+const two = 2;
+if (a === 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert('Invalid input data');
+} else {
+  let discriminant = b * b - four * a * c;
+  if (discriminant < 0) {
+    console.log('no solution');
+  } else if (discriminant === 0) {
+    console.log('x = 0');
+  } else {
+    const x1 = Math.round((-b + Math.sqrt(discriminant)) / (two * a));
+    const x2 = Math.round((-b - Math.sqrt(discriminant)) / (two * a));
+    console.log('x1 = ' + x1 + ' and x2 = ' + x2);
+  }
 }
-console.log(a);
-
-let b = parseFloat(
-    prompt('please enter "b" to solve quadratic equation (ax2 + bx + c = 0)')
-);
-if (b === "" || isNaN(b)) {
-    alert("‘Invalid input data’");
-}
-console.log(b);
-
-let c = parseFloat(
-    prompt('please enter "c" to solve quadratic equation (ax2 + bx + c = 0)')
-);
-if (c === "" || isNaN(c)) {
-    alert("‘Invalid input data’");
-}
-console.log(c);
-
-let dis = b * b - 4 * a * c;
-console.log(dis);
