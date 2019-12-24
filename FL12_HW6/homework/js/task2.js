7e7faddbc3105522467f71d9cd18e1aaa8dfaf79
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-12-24 20:09:05 +0200

    made some changes

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,22 +1,20 @@
-const a = parseInt(prompt("Side A"));
-const b = parseInt(prompt("Side B"));
-const c = parseInt(prompt("Side C"));
+let a = +prompt('Side A');
+let b = +prompt('Side B');
+let c = +prompt('Side C');
 
-if (a === "" || b === "" || c === "") {
-    alert("input values should be ONLY numbers");
+if (a === '' || b === '' || c === '') {
+  alert('input values should be ONLY numbers');
 } else if (isNaN(a) || isNaN(b) || isNaN(c)) {
-    alert("input values should be ONLY numbers");
-} else if (a === 0 || b === 0 || c === 0) {
-    alert("A triangle must have 3 sides with a positive definite length");
-}
-
-if (!(a + b > c && b + c > a && a + c > b) && (a === 0 || b === 0 || c === 0)) {
-    alert("Triangle doesn’t exist");
-    console.log("Triangle doesn’t exist");
+  alert('input values should be ONLY numbers');
+} else if (a <= 0 || b <= 0 || c <= 0) {
+  alert('A triangle must have 3 sides with a positive definite length');
 } else if (a === b && b === c) {
-    console.log("Equilateral triangle");
+  console.log('Equilateral triangle');
 } else if (a === b || a === c || b === c) {
-    console.log("Isosceles triangle");
+  console.log('Isosceles triangle');
+} else if (a !== b && a !== c && b !== c) {
+  console.log('Scalene triangle');
 } else {
-    console.log("Scalene triangle");
+  alert('Triangle doesn’t exist');
+  console.log('Triangle doesn’t exist');
 }
