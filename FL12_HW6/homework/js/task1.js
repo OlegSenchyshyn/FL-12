01d97a64ca67f5800253660a2fb33014f8a1c2bb
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2019-12-24 20:09:51 +0200

    Update task1.js

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,6 +1,6 @@
-const a = +prompt('Please enter value for "a" to solve Quadratic Equation (ax2 + bx + c = 0)');
-const b = +prompt('Please enter value for "b" to solve Quadratic Equation (ax2 + bx + c = 0)');
-const c = +prompt('Please enter value for "c" to solve Quadratic Equation (ax2 + bx + c = 0)');
+let a = +prompt('Please enter value for "a" to solve Quadratic Equation (ax2 + bx + c = 0)');
+let b = +prompt('Please enter value for "b" to solve Quadratic Equation (ax2 + bx + c = 0)');
+let c = +prompt('Please enter value for "c" to solve Quadratic Equation (ax2 + bx + c = 0)');
 const four = 4;
 const two = 2;
 if (a === 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
